b80a6104fab7adcf61219928306269b0653248bf
JeremyXZ <jeremyuk168@gmail.com>
2022-10-21 12:31:39 +0100

    router not working

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const recipesRouter = require('./routes/recipes.js')
+
+
 
 app.use(express.static("public"));
+app.use(recipesRouter)
 app.use(express.json());
 
 app.listen(PORT, () => {
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const recipesRouter = express.Router();
 // Write your router code here!
+recipesRouter.get("/", (req, res) => {
+    console.log("hello")
+    res.send("hi")
+})
+
+
 
 module.exports = recipesRouter;
